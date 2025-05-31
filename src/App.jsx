6c804560d4368eece2e01a00b21fa84965d68fc4
Adarsh Mohite <mohiteadarsh15@gmail.com>
2025-05-31 17:24:06 +0530

    Weather-App React js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const 
+app = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default 
+app
